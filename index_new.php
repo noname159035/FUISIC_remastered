b3ec1dbd6822126a057b1586ad85ac14cf70b7a4
Соловьёв Максим <mnsolovev@edu.hse.ru>
2023-05-16 13:23:25 +0300

    111

---
+++ index_new.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <!-- Иконки -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicons/site.webmanifest"/>
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbads5"/>
+
+        <link rel="stylesheet" href="style/index_new_style.css" />
+        <link rel="stylesheet" href="style/header_footer_style.css" />
+
+        <meta name="msapplication-TileColor" content="#2b5797"/>
+        <meta
+        name="theme-color" content="#ffffff"/>
+        <!--    <script src="main.js"></script>-->
+        <title>FUISIC</title>
+        <!-- Шрифты -->
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="conteiner">
+            <header>
+               <img src="style/img/test.svg" class="hdr logo">
+               <a href="#" class="hdr hdr_txt">Главная</a>
+               <p class="hdr hdr_txt pnt">•</p>
+               <a href="#" class="hdr hdr_txt">Подборки</a>
+               <p class="hdr hdr_txt pnt">•</p>
+               <a href="#" class="hdr hdr_txt">Тесты</a>
+               <p class="hdr hdr_txt pnt">•</p>
+               <a href="#" class="hdr hdr_txt">Помощь</a>
+               <button class="hdr btn">Войти</button>
+            </header>
+
+            <img src="style/img/Areas_main_page.svg" alt="" class="areas">
+
+            <div class="areas_conteiner">
+                <div class="fuisic_areas text">FUISIC</div>
+                <p class="fuisic_areas areas_top_text text">Тренажер по запоминанию формул</p>
+                <p class="fuisic_areas areas_bottom_text text">физики и математики</p>
+            </div>
+            <br>
+
+            <div>
+                <img src="style/img/three_bubbles.svg" alt="" class="three_bubbles">
+                <div class="bubble_cont">
+                    <p class="text bubble_text">Мы не только помогаем запоминать самые</p>
+                    <p class="text bubble_text">сложные законы с помощью особых</p>
+                    <p class="text bubble_text">алгоритмов, но и даем подробное</p>
+                    <p class="text bubble_text">объяснение</p>
+                </div>
+            </div>
+
+            <div class="separator"></div>
+
+            <div>
+                <div class="image_block">
+                    <img src="style/img/image_1.svg" alt="" id="image">
+                </div>
+                <div>
+                    <h1 class="text image_block_h">КАК РАБОТАЕТ FUISIC?</h1>
+                    <p class="image_block_text text image_text_1">Алгоритм, используемый Quizlet</p>
+                    <p class="image_block_text text image_text_2">для запоминания карточек, - это</p>
+                    <p class="image_block_text text image_text_3">алгоритм интервального</p>
+                    <p class="image_block_text text image_text_4">повторения.</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
